263a2932d974ffc786f60e2f200d8e771c97b462
Vladimir <vovan0503@mail.ru>
2021-02-02 18:30:49 +0300

    Fix slider btn

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -11,14 +11,26 @@ const Slider = ({ id, autoSliderInit, position, changePosition }) => {
 
     const [slideArr, setSlideArr] = React.useState([]);
     const [slideCount, setSlideCount] = React.useState(1);
-    // const [slideSize, setSlideSize] = React.useState({
-    //     width: '950px',
-    //     height: '440px'
-    // });
+    const [btnInit, setBtnInit] = React.useState(false);
 
     let interval = null;
 
     const dispatch = useDispatch();
+
+    const btnRef = React.useRef();
+
+    const widthScreen = document.body.clientWidth;
+
+    const initMouseOver = () => {
+        setBtnInit(true);
+    };
+
+    const initMouseOut = (e) => {
+        const path = e.path || (e.composedPath && e.composedPath());
+        if (!path.includes(btnRef.current)) {
+            setBtnInit(false);
+        };
+    };
 
     const loadFrames = async (id) => {
         try {
@@ -53,53 +65,29 @@ const Slider = ({ id, autoSliderInit, position, changePosition }) => {
         setSlideCount(slideCount === 0 ? slideArr.length - 1 : slideCount - 1);
     };
 
-    // const changeSlideSize = () => {
-    //     const windowWidth = document.body.clientWidth;
-    //     if (windowWidth <= 950) {
-    //         setSlideSize({
-    //             width: '100wv',
-    //             height: '440px'
-    //         })
-    //     }
-
-    //     console.log(windowWidth);
-    // }
-
-    // const sliderItems = (url, index) => (
-    //     {
-    //         width: size.width,
-    //         height: size.height,
-    //         marginTop: `${index > 0 ? `-${size.height}` : ''}`,
-    //         backgroundImage: `url(${url})`,
-    //         backgroundSize: 'cover',
-    //         borderRadius: '3px',
-    //         transition: '1.5s',
-    //         position: 'relative',
-    //         zIndex: index + 1,
-    //         opacity: `${index === slideCount ? '100%' : '0'}`
-    //     }
-    // );
-
     const slideStyles = (index) => (
         {
             margin: '0 auto',
             display: 'block',
-            // width: size.width,
-            // height: size.height,
             marginTop: `${index > 0 ? `-${position}` : ''}`,
             borderRadius: '3px',
             transition: '1.5s',
-            // position: 'relative',
             zIndex: index + 1,
             opacity: `${index === slideCount ? '100%' : '0'}`
         }
     );
 
-    console.log(position);
+    console.log(btnInit);
+    console.log(widthScreen);
 
     React.useEffect(() => {
         loadFrames(id);
         changePosition();
+        if (widthScreen <= 770) {
+            setBtnInit(true);
+        } else {
+            document.body.addEventListener('mouseover', initMouseOut);
+        };
     }, []);
 
     return (
@@ -109,38 +97,42 @@ const Slider = ({ id, autoSliderInit, position, changePosition }) => {
                     ? slideArr.map((frame, id) => <img key={id} src={frame.image} style={slideStyles(id)} />)
                     : <img src='https://argamak-sher.uz/wp-content/uploads/no-image.png'></img>}
             </div>
-            <div className={`sliderBtnBlock ${!slideArr.length ? 'hide' : ''}`}>
-                <button onClick={slideBack}>
-                    <svg
-                        className="left"
-                        width="50"
-                        height="50"
-                        viewBox="0 0 10 6"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
-                            fill="black"
-                        />
-                    </svg>
-                </button>
-                <button onClick={slideFoward}>
-                    <svg
-                        className="right"
-                        width="50"
-                        height="50"
-                        viewBox="0 0 10 6"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
-                            fill="black"
-                        />
-                        <g></g>
-                    </svg>
-                </button>
+            <div ref={btnRef} style={{ display: !slideArr.length ? 'hide' : '' }}>
+                <div onMouseOver={initMouseOver}
+                    className={`sliderBtnBlock ${btnInit ? 'showBtn' : ''}`}
+                >
+                    <button onClick={slideBack}>
+                        <svg
+                            className="left"
+                            width="50"
+                            height="50"
+                            viewBox="0 0 10 6"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
+                                fill="black"
+                            />
+                        </svg>
+                    </button>
+                    <button onClick={slideFoward}>
+                        <svg
+                            className="right"
+                            width="50"
+                            height="50"
+                            viewBox="0 0 10 6"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
+                                fill="black"
+                            />
+                            <g></g>
+                        </svg>
+                    </button>
+                </div>
             </div>
         </div>
     );
